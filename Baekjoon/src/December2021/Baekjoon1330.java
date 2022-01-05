24707d680f2bf1a0162cf26e3d704db523b792ce
genieinalamp <wlsgml9510@naver.com>
2022-01-05 20:18:08 +0900

    알고리즘

---
+++ Baekjoon/src/December2021/Baekjoon1330.java
@@ -0,0 +1,7 @@
+package December2021;
+
+public class Baekjoon1330 {
+    public static void main (String[] args) {
+        System.out.println("Hello World");
+    }
+}
---
+++ Baekjoon/src/December2021/Practice.java
@@ -0,0 +1,11 @@
+package December2021;
+
+import java.util.Scanner;
+
+//두 정수 A와 B가 주어졌을 때, A와 B를 비교하는 프로그램을 작성하시오.
+public class Practice {
+    public static void main(String[] args) {
+        Scanner number = new Scanner;
+        int number =
+    }
+}
